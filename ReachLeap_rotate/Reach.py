b1cd4c392f428aa26038b840f19423577fb5c20e
gilbertyap <gilbert.yap16@gmail.com>
2018-02-21 11:03:34 -0500

    Fixed averaging issue, more comments

--- ReachLeap_rotate/Reach.py
+++ ReachLeap_rotate/Reach.py
@@ -19,10 +19,16 @@ from servoLimit import changey
 from servoLimit import changez
 from servoLimit import changec
 
+
 ser = serial.Serial('COM1', 115200, timeout=1)
+
+# Initiate averages and counter (to starting position)
+average = {"x_avg": 0.0, "y_avg": 00.0, "z_avg": 0.0, "claw_avg": 0.0}
+average_factor = 3
 
 class SampleListener(Leap.Listener):
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
 
     def on_init(self, controller):
         print "Initialized"
@@ -36,6 +42,92 @@ class SampleListener(Leap.Listener):
 
     def on_exit(self, controller):
         print "Exited"
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+
+        thumb_x = 0.0
+        thumb_z = 0.0
+        index_x = 0.0
+        index_y = 0.0
+        index_z = 0.0
+        lower_index_x = 0.0
+        lower_index_z = 0.0
+
+        print "Frame id: %d,  hands: %d" % (frame.id, len(frame.hands))
+
+        if len(frame.hands) > 0:
+            # Figure out which hand has been there the longest
+            oldest_hand_time = 0.0
+            oldest_hand_id = 0
+
+            # Get the primary hand
+            for hand in frame.hands:
+                if hand.time_visible > oldest_hand_time:
+                    oldest_hand_time = hand.time_visible
+                    oldest_hand_id = hand.id
+
+            # Assign the oldest hand to be the hand that is tracked
+            if frame.hand(oldest_hand_id).is_valid:
+                track_hand = frame.hand(oldest_hand_id)
+
+                # Grab finger coordinate data and sum them over x number of frames
+                for finger in track_hand.fingers:
+                    bone = finger.bone(3)
+
+                    if self.finger_names[finger.type] == "Thumb":
+                        thumb_x += bone.next_joint[0]
+                        thumb_z += bone.next_joint[2]
+
+                    if self.finger_names[finger.type] == "Index":
+                        index_x += bone.next_joint[0]
+                        index_y += bone.next_joint[1]
+                        index_z += bone.next_joint[2]
+
+                    bone = finger.bone(2)
+                    if self.finger_names[finger.type] == "Index":
+                        lower_index_x += bone.prev_joint[0]
+                        lower_index_z += bone.prev_joint[2]
+
+                # print " id %d, position: %s" % (hand.id, hand.palm_position)
+
+            # Perform averaging every x frames and send over serial
+            if frame.id % average_factor == 0:
+                # Perform average on figure coordinates
+                thumb_x = thumb_x/average_factor
+                thumb_z = thumb_z/average_factor
+                index_x = index_x/average_factor
+                index_y = index_y/average_factor
+                index_z = index_z/average_factor
+                lower_index_x = lower_index_x/average_factor
+                lower_index_z = lower_index_x/average_factor
+
+                # Do coordinate to servo conversion on these averages
+                average["x_avg"] = changex(index_x, track_hand.wrist_position[0], index_z, track_hand.wrist_position[2])
+                average["y_avg"] = changey(index_y, track_hand.wrist_position[1], index_z, track_hand.wrist_position[2])
+                average["z_avg"] = changez(track_hand.palm_position[2])
+                average["claw_avg"] = changec(thumb_x, lower_index_x, thumb_z, lower_index_z)
+
+                print average
+
+                write_values = [240,  # Start ID
+                                241, average["x_avg"],  # base
+                                242, average["y_avg"],  # height
+                                243, average["z_avg"],  # reach
+                                244, average["claw_avg"],  # claw
+                                245]  # End ID
+
+                # Write all values to serial
+                ser.write(write_values)
+
+                # Reset averages for less filtering
+                average["x_avg"] = 0.0
+                average["y_avg"] = 0.0
+                average["z_avg"] = 0.0
+                average["claw_avg"] = 0.0
+
+            time.sleep(0.05)
 
     def state_string(self, state):
         if state == Leap.Gesture.STATE_START:
@@ -52,14 +144,6 @@ class SampleListener(Leap.Listener):
 
 
 def main():
-    cont = False
-    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-
-    # Initiate averages and counter (to starting position)
-    average = {"x_avg": 90.0, "y_avg": 100.0, "z_avg": 90.0, "claw_avg": 85.0}
-    frame_counter = 0
-    average_factor = 5
-
     # Create a sample listener and controller
     listener = SampleListener()
     controller = Leap.Controller()
@@ -67,97 +151,19 @@ def main():
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
 
-    # Wait until the controller is connected
-    while controller.is_connected is False:
-        time.sleep(0.01)
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Move arm to default position
+        ser.write(chr(254))
 
-    # Run the program indefinitely
-    while cont is False and controller.is_connected is True:
-        # For every average_factor frames, average out the coordinates
-        while frame_counter < average_factor:
-            frame = controller.frame()
-            thumb_x = 0.0
-            thumb_z = 0.0
-            index_x = 0.0
-            index_y = 0.0
-            index_z = 0.0
-
-            print "Frame id: %d, hands: %d" % (frame.id, len(frame.hands))
-
-            if frame.hands > 0:
-                # Figure out which hand has been there the longest
-                oldest_hand_time = 0.0
-                oldest_hand_id = 0
-
-                for hand in frame.hands:
-                    if hand.time_visible > oldest_hand_time:
-                        oldest_hand_time = hand.time_visible
-                        oldest_hand_id = hand.id
-
-                # Only track the hand with the id of the hand with the longest time visible
-                track_hand = frame.hand(oldest_hand_id)
-                for finger in track_hand.fingers:
-                    bone = finger.bone(3)
-                    if finger_names[finger.type] == "Thumb":
-                        thumb_x = bone.next_joint[0]
-                        # thumb_y = bone.next_joint[1]
-                        thumb_z = bone.next_joint[2]
-
-                    if finger_names[finger.type] == "Index":
-                        index_x = bone.next_joint[0]
-                        index_y = bone.next_joint[1]
-                        index_z = bone.next_joint[2]
-                        lower_index_x = bone.prev_joint[0]
-                        lower_index_z = bone.prev_joint[2]
-
-                    bone = finger.bone(2)
-                    if finger_names[finger.type] == "Index":
-                        lower_index_x = bone.prev_joint[0]
-                        lower_index_z = bone.prev_joint[2]
-
-                    print " id %d, position: %s" % (hand.id, hand.palm_position)
-
-                average["x_avg"] += changex(index_x, track_hand.wrist_position[0], index_z, track_hand.wrist_position[2])
-                average["y_avg"] += changey(index_y, track_hand.wrist_position[1], index_z, track_hand.wrist_position[2])
-                average["z_avg"] += changez(track_hand.palm_position[2])
-                average["claw_avg"] += changec(thumb_x, lower_index_x, thumb_z, lower_index_z)
-
-                time.sleep(0.01)
-                frame_counter += 1
-
-            # Average out the values, effectively a low pass filter
-            average["x_avg"] = int(average["x_avg"] / average_factor)
-            average["y_avg"] = int(average["y_avg"] / average_factor)
-            average["z_avg"] = int(average["z_avg"] / average_factor)
-            average["claw_avg"] = int(average["claw_avg"] / average_factor)
-
-        # Construct and send message
-        write_values = [240,  # Start ID
-                        241, chr(average["x_avg"]),  # base
-                        242, chr(average["y_avg"]),  # height
-                        243, chr(average["z_avg"]),  # reach
-                        244, chr(average["claw_avg"]),  # claw
-                        245]  # End ID
-
-        # Send messages over serial
-        print write_values
-        ser.write(write_values)
-
-        # Keep this process running until Enter is pressed
-        print "Press Enter to quit..."
-        try:
-            sys.stdin.readline()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            cont = True
-
-    # Move arm to default position
-    ser.write(chr(254))
-
-    time.sleep(2)
-    # Remove the sample listener when done
-    controller.remove_listener(listener)
+        time.sleep(2)
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
 
 
 if __name__ == "__main__":
--- ReachLeap_rotate/servoLimit.py
+++ ReachLeap_rotate/servoLimit.py
@@ -3,110 +3,119 @@
 # and the highest value that you want to use
 import math
 
-# NEED TO GET ANGLE VERSION OF THESE MEASUREMENTS, OR MIGHT NOT NEED THESE
+# TODO NEED TO GET ANGLE VERSION OF THESE MEASUREMENTS
 
 default_limits = {"xt": 150.0, "xb": -150.0,
                   "yt": 225.0, "yb": 0.0,
                   "zt": 75.0, "zb": -75.0,
                   "ct": 75.0, "cb": 45.0}
 
+# Servo limits
+# x - base, y - height, z - reach, c - claw
+servo_limits = {"xt": 180.0, "xb": 0.0,
+                "yt": 130.0, "yb": 20.0,
+                "zt": 145.0, "zb": 60.0,
+                "ct": 85, "cb": 10.0}
+
 current_dict = default_limits
 
 try:
     a_file = open('test_limits.txt', 'r')
-#    current_dict['xt'] = float(a_file.readline())
-#    current_dict['xb'] = float(a_file.readline())
-#    current_dict['yt'] = float(a_file.readline())
-#    current_dict['yb'] = float(a_file.readline())
+#   current_dict['xt'] = float(a_file.readline())
+#   current_dict['xb'] = float(a_file.readline())
+#   current_dict['yt'] = float(a_file.readline())
+#   current_dict['yb'] = float(a_file.readline())
     current_dict['zt'] = float(a_file.readline())
     current_dict['zb'] = float(a_file.readline())
     a_file.close()
 except IOError:
     print "Problems accessing file! Default limits used"
 
-# Servo limits
-servoXTLimit = 180.0  # Base servo top angle limit
 servoXDefault = 90.0
-servoXBLimit = 0.0  # Base servo bottom angle limit
-
-servoYTLimit = 130.0  # Height servo top angle limit
 servoYDefault = 90.0
-servoYBLimit = 20.0  # Height servo bottom angle limit
-
 palmZTLimit = current_dict['zt']  # top limit
 palmZBLimit = current_dict['zb']  # bottom limit
 palmZDelta = palmZTLimit - palmZBLimit  # Difference between the above
-servoZTLimit = 145.0  # Reach servo top angle limit
-servoZBLimit = 60.0  # Reach servo bottom angle limit
-
-servoCTLimit = 85.0  # Claw servo top angle limit
-servoCDefault = 85.0
-servoCBLimit = 10.0  # Claw servo bottom angle limit
-
-# Use Linear Interpolation to restrict movement
 
 
+# Changes x coordinate into value for the servo motor
 def changex(finger_x, wrist_x, finger_z, wrist_z):
+    # Determine whether or not to flip value (due to rotation)
     flip = -1.0 if finger_z >= wrist_z else 1.0
 
+    # Find the angle between the finger and wrist, establish top and bottom limits
     horizontal_angle = math.atan(-(wrist_x - finger_x)/(-flip*(finger_z - wrist_z)))*(180.0/math.pi)
-    # print(horizontal_angle)
-    angleXTLimit = servoXTLimit - servoXDefault
-    angleXBLimit = servoXDefault - servoXBLimit
+    angleXTLimit = servo_limits["xt"] - servoXDefault
+    angleXBLimit = servoXDefault - servo_limits["xb"]
 
+    # Determine if the value found is beyond the limits
+    # if not, move the position relative to the default
     if horizontal_angle > 0:
         if horizontal_angle >= angleXTLimit:
-            return int(servoXTLimit)
+            return int(servo_limits["xt"])
         else:
             return int(servoXDefault - horizontal_angle)
     else:
         if abs(horizontal_angle) >= angleXBLimit:
-            return int(servoXBLimit)
+            return int(servo_limits["xb"])
         else:
             return int(servoXDefault + abs(horizontal_angle))
 
 
+# Changes y coordinate into value for the servo motor
 def changey(finger_y, wrist_y, finger_z, wrist_z):
     flip = -1.0 if finger_z >= wrist_z else 1.0
     vertical_angle = math.atan((finger_y-wrist_y)/(-flip*(finger_z-wrist_z)))*(180/math.pi)
-    # print(vertical_angle)
-    angleYTLimit = servoYTLimit - servoYDefault
-    angleYBLimit = servoYDefault - servoYBLimit
+    angleYTLimit = servo_limits["yt"] - servoYDefault
+    angleYBLimit = servoYDefault - servo_limits["yb"]
 
     if vertical_angle > 0:
         if vertical_angle >= angleYTLimit:
-            return int(servoYTLimit)
+            return int(servo_limits["yt"])
         else:
             return int(servoYDefault + vertical_angle)
     else:
         if abs(vertical_angle) >= angleYBLimit:
-            return int(servoYBLimit)
+            return int(servo_limits["yb"])
         else:
             return int(servoYDefault - abs(vertical_angle))
 
 
+# Changes z coordinate into value for the servo motor
 def changez(palmz):
     if palmz < palmZBLimit:
-        return int(servoZTLimit)
+        return int(servo_limits["zt"])
     elif palmz > palmZTLimit:
-        return int(servoZBLimit)
+        return int(servo_limits["zb"])
     else:
-        return int(abs((((palmz - palmZBLimit) / palmZDelta) * servoZBLimit)
-                       + ((1.0 - abs((palmz - palmZBLimit) / palmZDelta)) * servoZTLimit)))
+        # The final value that is returned is the percentage the servo's
+        # bottom limit plus a percentage of the servo's top limit
+        # This is done by multiplying the limit by a ratio of the difference from the limit divided by the midpoint
+        return int(abs((((palmz - palmZBLimit) / palmZDelta) * servo_limits["zb"])
+                       + ((1.0 - abs((palmz - palmZBLimit) / palmZDelta)) * servo_limits["zt"])))
 
 
+# Changes claw coordinate into value for the servo motor
 def changec(finger1_x, finger2_x, finger1_z, finger2_z):
     fingerXDist = abs(finger1_x - finger2_x)
     fingerZDist = abs(finger1_z - finger2_z)
     fingerDist = math.sqrt(abs((fingerXDist ** 2) + (fingerZDist ** 2)))
     fingersCDelta = current_dict['ct'] - current_dict['cb']
-    # print fingerDist
 
     if fingerDist <= current_dict['cb']:
-        return int(servoCTLimit)
+        return int(servo_limits["ct"])
     elif fingerDist > current_dict['ct']:
-        return int(servoCBLimit)
+        return int(servo_limits["cb"])
     else:
-        return int(abs((((fingerDist - current_dict['cb']) / fingersCDelta) * servoCBLimit)
-                       + ((1.0 - abs((fingerDist - current_dict['cb']) / fingersCDelta)) * servoCTLimit)))
+        return int(abs((((fingerDist - current_dict['cb']) / fingersCDelta) * servo_limits["cb"])
+                       + ((1.0 - abs((fingerDist - current_dict['cb']) / fingersCDelta)) * servo_limits["ct"])))
 
+
+# Generic LERP function, value must be [0,1]
+def lerp(value, bot_limit, top_limit):
+    if value > top_limit:
+        return top_limit
+    elif value < bot_limit:
+        return bot_limit
+    else:
+        return ((1 - value)*bot_limit) + (value * top_limit)
